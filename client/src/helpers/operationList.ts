e7e1097f5bf7dd37ac17616902d6e57f6cf3b9b8
NAmaral <26823958+N-Amaral@users.noreply.github.com>
2023-02-23 19:02:37 +0000

    Working on pre Operation warning screen

--- client/src/helpers/operationList.ts
+++ client/src/helpers/operationList.ts
@@ -2,7 +2,7 @@ export const operationList: {
   PT: {
     levantamentos: { opTitles: string[]; opValues: string[] };
     consultas: string[];
-    transferencias: {};
+    transferencias: { opTitles: string[]; opValues: string[] };
     pagamentos: {};
     depositos: string[];
     sair: {};
@@ -14,7 +14,7 @@ export const operationList: {
       opValues: ["20", "40", "60", "100", "200", "0", "00"],
     },
     consultas: ["Consultas de Saldo", "Consulta de Movimentos de Conta", "Consulta de NIB e IBAN", "Outras Operações"],
-    transferencias: {},
+    transferencias: { opTitles: ["Outras Operações", "Transferências"], opValues: ["/", "/operations/"] },
     pagamentos: {},
     depositos: ["Depósitos na conta própria", "Depósitos em outra conta", "Outras Operações"],
     sair: {},
--- client/src/scenes/transfers/index.tsx
+++ client/src/scenes/transfers/index.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useContext } from "react";
 import { OperationContext } from "../../context/OperationContext";
 import OperationLayout from "../../components/OperationLayout";
+import MenuOptions from "../../components/MenuOptions";
 import Layout from "../layout";
 import { HeaderThreeText } from "../../components/HeaderText";
+import { operationList } from "../../helpers/operationList";
 type Props = {};
 
 const TransferPage = () => {
+  const titles: string[] = operationList.PT.transferencias.opTitles;
+  const values: string[] = operationList.PT.transferencias.opValues;
+
   return (
     <div className="flex flex-col items-center">
-      <div className="m-2  basis-1/4 p-5 uppercase">
+      <div className="m-2 basis-1/4 p-5 uppercase">
         <h1 className="text-3xl font-extrabold text-white">transferência multibanco</h1>
       </div>
       <div className="text m-2 basis-1/4 p-5 uppercase">
@@ -18,10 +23,11 @@ const TransferPage = () => {
         <HeaderThreeText>para outro banco - até às 15h de dia útil:</HeaderThreeText>
         <HeaderThreeText>crédito até ao 1º dia útil seguinte.</HeaderThreeText>
       </div>
-      <div className="m-2  basis-1/4 p-5 uppercase">
+      <div className="m-2  basis-1/4 pt-3 uppercase">
         <HeaderThreeText>para outro banco - depois das 15h ou em dia não útil:</HeaderThreeText>
         <HeaderThreeText>crédito até ao 2º dia útil seguinte.</HeaderThreeText>
       </div>
+      <MenuOptions opTitles={titles} opValues={values} />
     </div>
   );
 };
